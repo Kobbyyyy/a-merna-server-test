c5cb9d5ceab425186146d60c04546fad0f44e90c
Nana Kwabena Inkum <akotoinkumkobby@gmail.com>
2026-03-01 21:01:23 -0600

    Dice API Server

--- index.js
+++ index.js
@@ -12,36 +12,74 @@ const minorVersion = 3
 
 // Use Express to publish static HTML, CSS, and JavaScript files that run in the browser. 
 app.use(express.static(__dirname + '/static'))
+
 app.use(cors({ origin: '*' }))
 
-// The app.get functions below are being processed in Node.js running on the server.
-// Implement a custom About page.
+
+// LUDO DICE ROLLER ENDPOINTS
+
+
+// Wake-up endpoint
+app.get('/api/wakeup', (request, response) => {
+    console.log('Ludo server waking up at:', new Date().toISOString());
+    response.json({
+        status: 'awake',
+        message: 'Ludo server is ready',
+        timestamp: new Date().toISOString()
+    });
+});  
+
+// Dice roll endpoint - GET
+app.get('/api/roll', (request, response) => {
+    console.log('Rolling Ludo die...');
+    const roll = Math.floor(Math.random() * 6) + 1;
+    response.json({
+        value: roll,
+        isSix: roll === 6,
+        timestamp: new Date().toISOString()
+    });
+});
+
+// Dice roll endpoint  (for CORS demo)
+app.post('/api/roll', (request, response) => {
+    response.json({
+        message: 'POST request - used for CORS demo',
+        note: 'This triggers CORS preflight'
+    });
+});
+
+
+
+
+// About page
 app.get('/about', (request, response) => {
 	console.log('Calling "/about" on the Node.js server.')
 	response.type('text/plain')
 	response.send('About Node.js on Azure Template.')
 })
 
+// Version
 app.get('/version', (request, response) => {
 	console.log('Calling "/version" on the Node.js server.')
 	response.type('text/plain')
 	response.send('Version: '+majorVersion+'.'+minorVersion)
 })
 
+// Ping
 app.get('/api/ping', (request, response) => {
 	console.log('Calling "/api/ping"')
 	response.type('text/plain')
 	response.send('ping response')
 })
 
-// Return the value of 2 plus 2.
+// 2plus2
 app.get('/2plus2', (request, response) => {
 	console.log('Calling "/2plus2" on the Node.js server.')
 	response.type('text/plain')
 	response.send('4')
 })
 
-// Add x and y which are both passed in on the URL. 
+// Add two integers
 app.get('/add-two-integers', (request, response) => {
 	console.log('Calling "/add-two-integers" on the Node.js server.')
 	var inputs = url.parse(request.url, true).query
@@ -52,7 +90,7 @@ app.get('/add-two-integers', (request, response) => {
 	response.send(sum.toString())
 })
 
-// Template for calculating BMI using height in feet/inches and weight in pounds.
+// Calculate BMI
 app.get('/calculate-bmi', (request, response) => {
 	console.log('Calling "/calculate-bmi" on the Node.js server.')
 	var inputs = url.parse(request.url, true).query
@@ -63,41 +101,26 @@ app.get('/calculate-bmi', (request, response) => {
 	console.log('Height:' + heightFeet + '\'' + heightInches + '\"')
 	console.log('Weight:' + weight + ' lbs.')
 
-	// Todo: Implement unit conversions and BMI calculations.
-	// Todo: Return BMI instead of Todo message.
-
 	response.type('text/plain')
 	response.send('Todo: Implement "/calculate-bmi"')
 })
 
-// Test a variety of functions.
+// Test function
 app.get('/test', (request, response) => {
-    // Write the request to the log. 
     console.log(request);
-
-    // Return HTML.
     response.writeHead(200, {'Content-Type': 'text/html'});
     response.write('<h3>Testing Function</h3>')
-
-    // Access function from a separate JavaScript module.
     response.write("The date and time are currently: " + dt.myDateTime() + "<br><br>");
-
-    // Show the full url from the request. 
     response.write("req.url="+request.url+"<br><br>");
-
-    // Suggest adding something tl the url so that we can parse it. 
     response.write("Consider adding '/test?year=2017&month=July' to the URL.<br><br>");
     
-	// Parse the query string for values that are being passed on the URL.
 	var q = url.parse(request.url, true).query;
     var txt = q.year + " " + q.month;
     response.write("txt="+txt);
-
-    // Close the response
     response.end('<h3>The End.</h3>');
 })
 
-// Return Batman as JSON.
+// Batman JSON
 const batMan = {
 	"firstName":"Bruce",
 	"lastName":"Wayne",
@@ -129,24 +152,21 @@ app.get('/batman', (request, response) => {
 	response.send(JSON.stringify(batMan))
 })
 
-// Load your JSON data
+// Favorite Places
 const favoritePlaces = require('./FavoritePlaces.json');
 
-// Create a route that serves the JSON data
 app.get('/api/favorite-places', (req, res) => {
   res.json(favoritePlaces);
 });
 
-
-
-// Custom 404 page.
+// Custom 404 page
 app.use((request, response) => {
   response.type('text/plain')
   response.status(404)
   response.send('404 - Not Found')
 })
 
-// Custom 500 page.
+// Custom 500 page
 app.use((err, request, response, next) => {
   console.error(err.message)
   response.type('text/plain')
@@ -154,6 +174,7 @@ app.use((err, request, response, next) => {
   response.send('500 - Server Error')
 })
 
+// Start server
 app.listen(port, () => console.log(
   `Express started at \"http://localhost:${port}\"\n` +
   `press Ctrl-C to terminate.`)
